9de8e5dd99e43096b6fa90675d4576461875e74a
joseim-dev <joseimdev@gmail.com>
2025-04-23 16:34:54 +0900

    adsense 코드 스니펫 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7622300846359900"
+          crossorigin="anonymous"
+        ></script>
+      </head>
+
       <body className={`${fredoka.variable} antialiased`}>
         <TimerProvider>{children}</TimerProvider>
       </body>
