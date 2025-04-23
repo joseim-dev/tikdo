95f1e16d59ebabe864f21ae981f0f49f82a4137f
joseim-dev <joseimdev@gmail.com>
2025-04-23 16:37:37 +0900

    adsense 코드 스니펫 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7622300846359900"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         ></script>
       </head>
 
